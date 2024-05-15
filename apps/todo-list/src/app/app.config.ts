bb55be7f5d030feda5dc66342bc7b1264b863acf
vladimir.l@artinii.com <vladimir.l@artinii.com>
2024-05-15 14:55:31 +0200

    NgRx store, effects, entity added

--- apps/todo-list/src/app/app.config.ts
+++ apps/todo-list/src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRouter(appRoutes), provideStore(), provideEffects()],
 };
